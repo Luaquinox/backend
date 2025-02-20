ede497466bfc129b6a401b6b979d99b90f2819c3
Luaquinox <160145426+Luaquinox@users.noreply.github.com>
2025-02-20 09:12:20 -0300

    Add files via upload

--- lampada.js
+++ lampada.js
@@ -1,3 +1,17 @@
 const lamp = document.getElementById('lamp'); 
 const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff')
+
+function isLampBroken(){
+    return lamp.scr.inderOff('quebrada')> -1
+}
+function lampOn(){
+    if(!isLampBroken()){
+        lamp.scr= '.ligada.jpg';
+    }
+}
+    function lampOff(){
+    if(!isLampBroken()){
+        lamp.scr= '.desligada.jpg';
+    }
+}
